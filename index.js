45f1d64ab95030183dfa0b5897bdde2241d491e3
Hnesco <hannes.pohjola@gmail.com>
2020-11-29 22:35:23 +0200

    random again

--- index.js
+++ index.js
@@ -102,5 +102,5 @@ app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${PORT}`)
 })
