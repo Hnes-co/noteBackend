d1261ffe856b48cda09bc25aa500ecd5fbac79c9
Hnesco <hannes.pohjola@gmail.com>
2020-11-30 20:40:30 +0200

    all committ

---
+++ build/precache-manifest.5ba730fd1cf9674e53869fc5e446d484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc41e96338cb0983800ea99e177df56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b6950f2d975ce5f060a",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b6950f2d975ce5f060a",
+    "url": "/static/js/main.26b415e4.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
