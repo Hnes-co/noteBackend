fc4832ec236fd30f5c842b482dbe733789e29771
Hnesco <hannes.pohjola@gmail.com>
2020-12-01 17:16:18 +0200

    mongo ready for heroku

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const { response } = require('express')
+require('dotenv').config()
+const { response, request } = require('express')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
-
-
-
-
 
 app.use(express.json())
 
@@ -75,10 +72,9 @@ app.get('/api/notes/:id', (req, res) => {
 })
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    res.status(204).end()
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
 })
 
 const generateId = () => {
@@ -91,26 +87,27 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
+    if (body.content === undefined) {
       return response.status(400).json({ 
         error: 'content missing' 
       })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
-      id: generateId(),
-    }
+    })
   
-    notes = notes.concat(note)
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
