32a071915c3051476e9701b5e66d7311dee8942b
Hnesco <hannes.pohjola@gmail.com>
2020-12-01 21:18:51 +0200

    uibuild

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
 
+app.use(express.static('build'))
 app.use(express.json())
-
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -16,36 +16,9 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(express.static('build'))
-app.use(cors())
 app.use(requestLogger)
+app.use(cors())
 
-/*
-let notes = [  
-    {    
-        id: 1,    
-        content: "HTML is easy",    
-        date: "2020-01-10T17:30:31.098Z",    
-        important: true  
-    },  
-    {    
-        id: 2,    
-        content: "Browser can execute only Javascript",    
-        date: "2020-01-10T18:39:34.091Z",    
-        important: false  
-    },  
-    {    
-        id: 3,    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        date: "2020-01-10T19:20:14.298Z",    
-        important: true  
-    }
-]
-*/
 /*
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -58,33 +31,29 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        res.json(note)
-    }
-    else {
-        res.status(404).end()
-    }
-    
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
+app.get('/api/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id).then(note => {
+        if(note) {
+            res.json(note)
+        }
+        else {
+            res.status(404).end()
+        }
+        
     })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-}
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
   
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
   
     if (body.content === undefined) {
@@ -100,11 +69,49 @@ app.post('/api/notes', (request, response) => {
     })
   
     note.save().then(savedNote => {
-        response.json(savedNote)
+        response.json(savedNote.toJSON())
     })
+    .catch(error => next(error))
 })
 
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+    })
+      .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {    
+      return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 
